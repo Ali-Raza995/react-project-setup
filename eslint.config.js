aec925c7892a91f82d26adc1a17a717e9a22a0af
Salman <salmansidd991@gmail.com>
2024-08-17 13:15:39 +0500

    Initial Project Setup commit

---
+++ eslint.config.js
@@ -0,0 +1,98 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import typescript from '@typescript-eslint/eslint-plugin';
+import tailwindcss from 'eslint-plugin-tailwindcss';
+import prettier from 'eslint-plugin-prettier';
+
+export default [
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+    },
+    settings: {
+      react: { version: '18.3' },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      '@typescript-eslint': typescript,
+      'tailwindcss': tailwindcss,
+      'prettier': prettier,
+    },
+    rules: {
+      // JavaScript & TypeScript
+      ...js.configs.recommended.rules,
+      ...typescript.configs.recommended.rules,
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/explicit-module-boundary-types': 'error',
+      '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+      '@typescript-eslint/ban-ts-comment': 'error',
+
+      // React
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      'react/jsx-no-target-blank': 'error',
+      'react/prop-types': 'off',
+      'react/jsx-boolean-value': 'error',
+      'react/no-array-index-key': 'warn',
+      'react/jsx-curly-brace-presence': ['error', { props: 'never', children: 'never' }],
+
+      // React Hooks
+      ...reactHooks.configs.recommended.rules,
+      'react-hooks/exhaustive-deps': 'error',
+
+      // TailwindCSS
+      'tailwindcss/classnames-order': 'error',
+      'tailwindcss/no-custom-classname': 'error',
+
+      // Prettier
+      'prettier/prettier': ['error', {
+        bracketSpacing: true,
+        printWidth: 80,
+        singleQuote: true,
+        trailingComma: 'all',
+        tabWidth: 2,
+        useTabs: false,
+      }],
+
+      // Custom rules for stricter code quality
+      'eqeqeq': ['error', 'always'],
+      'no-console': 'warn',
+      'no-debugger': 'error',
+      'curly': 'error',
+      'no-multi-spaces': 'error',
+      'array-callback-return': 'error',
+      'consistent-return': 'error',
+      'no-unused-expressions': 'error',
+      'no-shadow': 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'prefer-arrow-callback': 'error',
+      'object-shorthand': 'error',
+      'arrow-body-style': ['error', 'as-needed'],
+    },
+    extends: [
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:prettier/recommended',
+      'plugin:tailwindcss/recommended',
+    ],
+  },
+];
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <h1 className='text-3xl text-center'>
+      Spector Rags Sass
+    </h1>
+  );
+}
+
+export default App;
